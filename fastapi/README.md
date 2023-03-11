e9adf868dd790ca4ad7ea1a6500b4bbff1a2192f
Rob Brennan <rob@therobbrennan.com>
2023-03-11 13:25:45 -0800

    Add example to read in a CSV file and create a scatter plot

--- fastapi/README.md
+++ fastapi/README.md
@@ -27,6 +27,7 @@ Let's refer to the `How to create an API in Python with FastAPI` section in the 
 (.venv) % pip install fastapi
 (.venv) % pip install uvicorn
 (.venv) % pip install pandas
+(.venv) % pip install matplotlib
 
 # Let's start our FastAPI server - Available at http://127.0.0.1:8000/
 (.venv) % uvicorn main:app --reload
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
 import pandas as pd
+
+import io
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+
+from fastapi import FastAPI, Response, BackgroundTasks
 
 app = FastAPI()
 
@@ -21,3 +27,25 @@ def get_iris():
     iris = pd.read_csv(url)
 
     return iris
+
+@app.get("/plot-iris")
+def plot_iris(background_tasks: BackgroundTasks):
+
+    img_buf = create_iris_image_buffer()
+    background_tasks.add_task(img_buf.close)
+    headers = {'Content-Disposition': 'inline; filename="out.png"'}
+    return Response(img_buf.getvalue(), headers=headers, media_type='image/png')
+
+def create_iris_image_buffer():
+    url ='https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+    iris = pd.read_csv(url)
+
+    # Create a 750px x 350px plot
+    plt.rcParams['figure.figsize'] = [7.50, 3.50]
+    plt.rcParams['figure.autolayout'] = True
+    fig = plt.figure()  # make sure to call this, in order to create a new figure
+    img_buf = io.BytesIO()
+    plt.scatter(iris['sepal_length'], iris['sepal_width'])
+    plt.savefig(img_buf, format='png')
+    plt.close(fig)
+    return img_buf

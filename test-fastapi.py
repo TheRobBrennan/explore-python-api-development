03e7b520a50102f435b7fb542a66feb1bf96d4b2
Rob Brennan <rob@therobbrennan.com>
2023-03-11 13:31:58 -0800

    Verify our original API example works as expected

--- test-fastapi.py
+++ test-fastapi.py
@@ -7,3 +7,7 @@ resp = requests.get('http://127.0.0.1:8000/plot-iris')
 file = io.BytesIO(resp.content)
 im = Image.open(file)
 im.show()
+
+# Let's try the original endpoint using the name query parameter
+resp = requests.get('http://127.0.0.1:8000/my-first-api?name=Rob')
+resp.text

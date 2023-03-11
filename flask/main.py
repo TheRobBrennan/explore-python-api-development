d50dff79765e41904152702ca316b42a84f36fe8
Rob Brennan <rob@therobbrennan.com>
2023-03-11 14:01:09 -0800

    Add /my-first-api Flask route

--- flask/main.py
+++ flask/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello, world! This is a web app using Python Flask!'
 
+@app.route('/my-first-api')
+def hello():
+    return "Hello world!"
+
 # Start our server
 app.run(debug=True, port=8000)

48e51d9abe87c59ec30725652f675fe7617ba517
Rob Brennan <rob@therobbrennan.com>
2023-03-11 12:59:01 -0800

    Add example where name is an optional query string parameter

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,5 +2,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/my-first-api")
-def hello(name: str): # http://127.0.0.1:8000/my-first-api?name=Rob
-  return {'Hello ' + name + '!'} 
+def hello(name = None):
+
+    if name is None:
+        text = 'Hello!'
+
+    else:
+        text = 'Hello ' + name + '!'
+
+    return text

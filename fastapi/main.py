7a20bb0abc69368cb76bb0d764d2d07851f3871c
Rob Brennan <rob@therobbrennan.com>
2023-03-11 12:56:57 -0800

    Add example to require a name query string parameter

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/my-first-api")
-def hello():
-  return {"Hello world!"}
+def hello(name: str): # http://127.0.0.1:8000/my-first-api?name=Rob
+  return {'Hello ' + name + '!'} 
